6dd142eb18e0ea6e47c3bc8a1cd4a1ab923400eb
unlockth <1392475180@qq.com>
2019-08-06 20:59:41 +0800

    Net基础第六天新增结构的项目

---
+++ Net基础第六天/05结构/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05结构
+{
+    public struct Person
+    {
+        public string _name; // 字段,前面需要添加下划线
+        public int _age;
+        public Gender _gender;
+    }
+    public enum Gender
+    {
+        男,
+        女
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //XX大学管理系统
+            //姓名、性别、年龄、年级
+            //string zsName = "张三";
+            //int zsAge = 18;
+            //char zsGender = '男';
+            //int zsGrade = 3;
+
+
+            Person zsPerson;
+            zsPerson._name = "张三";
+            zsPerson._age = 18;
+            zsPerson._gender = Gender.女;
+
+            Person lsPerson;
+            lsPerson._name = "李四";
+            lsPerson._age = 22;
+            lsPerson._gender = Gender.女;
+
+            Console.WriteLine(zsPerson._name);
+            Console.WriteLine(lsPerson._name);
+            Console.ReadKey();
+        }
+    }
+}
